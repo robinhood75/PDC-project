7852a6de45aeb0fea59eaa735f040033704453e9
robinhood75 <72042118+robinhood75@users.noreply.github.com>
2021-05-19 22:58:42 +0200

    Update transmitter.py

--- transmitter.py
+++ transmitter.py
@@ -13,10 +13,10 @@ def transmitter_ascii(s, N0, write=True):
     for i, bitstr in enumerate(str_to_binary):
         if len(bitstr) < 8:
             str_to_binary[i] = (8-len(bitstr))*'0' + bitstr
+    str_to_binary = ''.join(str_to_binary)
 
-    # Duplicate each sequence of 8 bits N0 times
-    str_to_binary = [bitstr*N0 for bitstr in str_to_binary]
-    input = ''.join(str_to_binary)
+    # Replicate each bit N0 times
+    input = ''.join([bit*N0 for bit in str_to_binary])
     input = list(map(int,input))
     input = [2*x-1 for x in input]
 
@@ -29,6 +29,6 @@ def transmitter_ascii(s, N0, write=True):
             f.write(str(bit)+" ")
         f.close()
 
-s = "hello, my name is Arthur"
-N0=2
+s = "hello, my name is Arthur, and I'm 22. I know, this is kind of old"
+N0=90 # WARNING: N0 should be multiple of 3
 transmitter_ascii(s, N0)
