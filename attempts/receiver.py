46ab3ce27e03795c3318c8dbe889f98e3877b15c
Amadeous <julien@biefer.me>
2021-06-03 11:54:37 +0200

    New structure and tried something

---
+++ attempts/receiver.py
@@ -0,0 +1,250 @@
+import numpy as np
+
+N0=30
+assert N0 % 3 == 0
+ENDIANNESS = 'big'
+
+def estimate_H():
+    """
+    Estimate H by outputing min average y**2 among i=0,1,2
+    Works independantly of the transmitter design
+    """
+    out = np.loadtxt("output.txt")
+    n = out.shape[0]
+    n_eff = n-n%3
+    MAP = np.zeros(3)
+    for i, y in enumerate(out[:n_eff]):
+        MAP[i%3] += y**2
+    MAP /= n_eff/3
+    return np.argmin(MAP), MAP
+
+def bin_to_dec(lst):
+    """
+    Input: np.array of bits
+    Output: associated decimal
+    """
+    lst = np.flip(lst)
+    dec = 0
+    for k, bit in enumerate(lst):
+        dec += bit*2**k
+    return dec.item()
+
+def load_signal(N0, filename='output.txt'):
+    # Load the received signal
+    out = np.loadtxt(filename)
+    n = out.shape[0]
+    if n % N0 !=0:
+        raise ValueError("Transmitted signal has incorrect length")
+    return (out, n)
+
+def decode_ascii(N0, H_hat):
+    """
+    Decodes the signal under ascii transmitter design:
+    Take the sign of the average over 2*N0/3 transmissions of the same bit
+    """
+    out = load_signal(N0)
+    n = out.shape[0]
+
+    # Drop the useless data
+    out_clean = []
+    for i, y in enumerate(out):
+        if i%3 != H_hat: out_clean.append(y)
+    if N0%3 != 0:
+        return "WARNING: N0 should be multiple of 3"
+    N0_eff = int(2*N0/3)
+
+    # Make a prediction for the n/N0 original bits
+    averages = [sum(out_clean[k*N0_eff:(k+1)*N0_eff]) for k in range(int(n/N0))]
+    decoded_bits = np.sign(np.array(averages))
+    decoded_bits = (decoded_bits+1)/2
+    decoded_bits = decoded_bits.astype(int)
+    
+    # Retreive the original string
+    decoded_ints = [bin_to_dec(decoded_bits[8*k:8*(k+1)])%128 for k in range(int(len(decoded_bits)/8))] # %ideally %128 to be discarded
+    decoded_string = ''.join([x.to_bytes(1,ENDIANNESS).decode('ascii') for x in decoded_ints])
+
+    return decoded_string
+
+def decode_utf8(N0, H_hat):
+    """
+    Decodes the signal under utf-8 transmitter design:
+    Takes the sign of the average over 2*N0/3 transmissions of the same bit
+    """
+    if N0 % 3 != 0:
+        raise ValueError('Warning: N0 should be multiple of 3')
+    N0_eff = 2*N0//3
+
+    out, n = load_signal(N0)
+
+    out_clean = [out[i] for i in range(len(out)) if i % 3 != H_hat]
+    if len(out_clean) % N0_eff != 0:
+        raise ValueError('Warning: length of out_clean should be multiple of N0_eff')
+
+    averages = [sum(out_clean[k*N0_eff : (k+1)*N0_eff]) for k in range(n//N0)]
+    
+    # Map to -1, 0 or 1
+    decoded_bits = np.sign(np.array(averages))
+    
+    # Map to binary
+    decoded_bits = ((decoded_bits+1)/2).astype(int)
+    decoded_ints = [int(''.join(decoded_bits[8*i:8*(i+1)].astype(str)), 2) for i in range(len(decoded_bits)//8)]
+    decoded_bytes = bytes(decoded_ints)
+    
+    print(f"Decoded bytes: {decoded_bytes}", f"{len(decoded_bytes)} bytes")
+
+    decoded_string = decoded_bytes.decode('utf-8')
+
+    return decoded_string
+
+def decode_utf8_8bits(N0, H_hat):
+    """
+    Decodes the signal under an utf-8 transmitter design optimized for 8bits utf-8:
+    Takes the sign of the average over 2*N0/3 transmissions of the same bit
+    """
+    if N0 % 3 != 0:
+        raise ValueError('Warning: N0 should be multiple of 3')
+    N0_eff = 2*N0//3
+
+    out, n = load_signal(N0)
+
+    out_clean = [out[i] for i in range(len(out)) if i % 3 != H_hat]
+    if len(out_clean) % N0_eff != 0:
+        raise ValueError('Warning: length of out_clean should be multiple of N0_eff')
+
+    averages = [sum(out_clean[k*N0_eff : (k+1)*N0_eff]) for k in range(n//N0)]
+
+    # Map to -1, 0 or 1
+    decoded_bits = np.sign(np.array(averages))
+
+    # Map to binary
+    decoded_bits = ((decoded_bits+1)/2).astype(int)
+
+    decoded_ints = [int(''.join(decoded_bits[7*i:7*(i+1)].astype(str)), 2) for i in range(len(decoded_bits)//7)]
+    decoded_bytes = bytes(decoded_ints)
+
+    print(f"Decoded bytes:\n{decoded_bytes}", f"{len(decoded_bytes)} bytes")
+
+    decoded_string = decoded_bytes.decode('utf-8')
+
+    return decoded_string
+
+dic = {
+    '1000': "e",
+    '1001': " ",
+    '1010': "t",
+    '1011': "a",
+    '1100': "o",
+    '1101': "i",
+    '1110': "n",
+    '1111': "s"
+    }
+
+def decode_with_dic(N0, H_hat, dic):
+    if N0 % 3 != 0:
+        raise ValueError('Warning: N0 should be multiple of 3')
+    N0_eff = 2*N0//3
+
+    out, n = load_signal(N0)
+
+    out_clean = [out[i] for i in range(len(out)) if i % 3 != H_hat]
+    if len(out_clean) % N0_eff != 0:
+        raise ValueError('Warning: length of out_clean should be multiple of N0_eff')
+
+    averages = [sum(out_clean[k*N0_eff : (k+1)*N0_eff]) for k in range(n//N0)]
+
+    # Map to -1, 0 or 1
+    decoded_bits = np.sign(np.array(averages))
+
+    # Map to binary
+    decoded_bits = ((decoded_bits+1)/2).astype(int)
+
+    # Decode using a 3-states automaton
+    decoded_string = []
+    state = 'expecting first bit'
+    collect_8 = collect_4 = []
+
+    for b in decoded_bits:
+        current_state = state
+        if current_state == 'expecting first bit':
+            if b==1:
+                state = 'expecting 4'
+                collect_4 = [1]
+            if b==0:
+                state = 'expecting 8'
+                collect_8 = [0]
+        elif current_state == 'expecting 4':
+            collect_4.append(b)
+            if len(collect_4) == 4:
+                pack4 = ''.join(list(map(str,collect_4)))
+                decoded_string.append(dic[pack4])
+                state = 'expecting first bit'
+        elif current_state == 'expecting 8':
+                collect_8.append(b)
+                if len(collect_8) == 8:
+                   decoded_int = bin_to_dec(collect_8)
+                   decoded_byte = decoded_int.to_bytes(1,ENDIANNESS)
+                   decoded_string.append(decoded_byte.decode('utf8'))
+                   state = 'expecting first bit'
+
+    print("Finished decoding with state: {}\n".format(state))
+
+    return ''.join(decoded_string)
+
+def decode_utf8_8bits_adapted(H_hat, N0_4, N0_3):
+
+    # Might be useless if N0_4 and N0_3 are differently used.
+    if N0_4 % 3 != 0 or N0_3 % 3 != 0:
+        raise ValueError('Warning: N0_i values should be multiple of 3')
+    
+    N0_4_eff = 2*N0_4//3
+    N0_3_eff = 2*N0_3//3
+    N0_eff = N0_4_eff + N0_3_eff
+
+    out, n = load_signal(N0_4 + N0_3)
+
+    out_clean = [out[i] for i in range(len(out)) if i % 3 != H_hat]
+    if len(out_clean) % N0_eff != 0:
+        raise ValueError('Warning: length of out_clean should be multiple of N0_eff')
+    
+    nbWords = n // (N0_4*16 + N0_3*8)
+    wordLen = N0_4_eff*16 + N0_3_eff*8
+    assert len(out_clean) == nbWords*wordLen
+    print(f"Found {nbWords} words!")
+
+    words_duplicated = [out_clean[i*wordLen:(i+1)*wordLen] for i in range(nbWords)]
+
+    def average(x, nbDuplicates):
+        nbSingleElement = len(x)//nbDuplicates
+        return np.array([np.sign(np.array(sum(x[i*nbDuplicates:(i+1)*nbDuplicates]))) for i in range(nbSingleElement)]).astype(int)
+
+    words_splitted = [(average(x[:16*N0_4_eff], N0_4_eff), average(x[-8*N0_3_eff:], N0_3_eff)) for x in words_duplicated]
+    for w in words_splitted:
+        s4 = sum(w[0])
+        s3 = sum(w[1])
+        print(w, '\t-> Error' if s4 != -15 or s3 != -7 else '')
+
+index, H_MAP = estimate_H()
+print("Deleted index: {} (from the following estimate: {})\n".format(index, H_MAP))
+
+decoded_string = decode_utf8_8bits_adapted(index, N0, N0)
+print("Decoded string using N0={}:".format(N0), decoded_string)
+
+original_message = None
+with open('message.txt', 'rb') as f:
+    original_message = f.read().decode('utf-8').strip()
+
+if original_message:
+    print("Original message was: '", original_message, "'", sep='')
+
+    errors = dict()
+    for i in range(len(original_message)):
+        if original_message[i] != decoded_string[i]:
+            errors[i] = {'original': original_message[i], 'decoded': decoded_string[i] }
+
+    print(f"Total number of errors: {len(errors)}")
+    for i,e in errors.items():
+        print(f' - index {i}:')
+        print('\texpected this: {0:08b}'.format(ord(e["original"])), end='')
+        print(f' -> {e["original"].encode("utf-8")} -> {e["original"]}')
+        print('\tand got this : {0:08b}'.format(ord(e["decoded"])), end='')
+        print(f' -> {e["decoded"].encode("utf-8")} -> {e["decoded"]}')
--- receiver.py
+++ receiver.py
@@ -1,218 +1,52 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 
-N0=129
-assert N0 % 3 == 0
-ENDIANNESS = 'big'
-
-def estimate_H():
+def remove_index(out):
     """
-    Estimate H by outputing min average y**2 among i=0,1,2
-    Works independantly of the transmitter design
+    remove deleted index
     """
-    out = np.loadtxt("output.txt")
     n = out.shape[0]
     n_eff = n-n%3
-    MAP = np.zeros(3)
+    estim = np.zeros(3)
     for i, y in enumerate(out[:n_eff]):
-        MAP[i%3] += y**2
-    MAP /= n_eff/3
-    return np.argmin(MAP), MAP
-
-def bin_to_dec(lst):
-    """
-    Input: np.array of bits
-    Output: associated decimal
-    """
-    lst = np.flip(lst)
-    dec = 0
-    for k, bit in enumerate(lst):
-        dec += bit*2**k
-    return dec.item()
-
-def load_signal(N0, filename='output.txt'):
-    # Load the received signal
-    out = np.loadtxt(filename)
-    n = out.shape[0]
-    if n % N0 !=0:
-        raise ValueError("Transmitted signal has incorrect length")
-    return (out, n)
-
-def decode_ascii(N0, H_hat):
-    """
-    Decodes the signal under ascii transmitter design:
-    Take the sign of the average over 2*N0/3 transmissions of the same bit
-    """
-    out = load_signal(N0)
-    n = out.shape[0]
-
-    # Drop the useless data
+        estim[i%3] += y**2
+    estim /= n_eff/3
+    H_hat = np.argmin(estim)
+    
     out_clean = []
     for i, y in enumerate(out):
-        if i%3 != H_hat: out_clean.append(y)
-    if N0%3 != 0:
-        return "WARNING: N0 should be multiple of 3"
-    N0_eff = int(2*N0/3)
+        if i%3 != H_hat:
+            out_clean.append(y)
+            
+    return out_clean, H_hat
 
-    # Make a prediction for the n/N0 original bits
-    averages = [sum(out_clean[k*N0_eff:(k+1)*N0_eff]) for k in range(int(n/N0))]
-    decoded_bits = np.sign(np.array(averages))
-    decoded_bits = (decoded_bits+1)/2
-    decoded_bits = decoded_bits.astype(int)
-    
-    # Retreive the original string
-    decoded_ints = [bin_to_dec(decoded_bits[8*k:8*(k+1)])%128 for k in range(int(len(decoded_bits)/8))] # %ideally %128 to be discarded
-    decoded_string = ''.join([x.to_bytes(1,ENDIANNESS).decode('ascii') for x in decoded_ints])
-
-    return decoded_string
-
-def decode_utf8(N0, H_hat):
+def remove_noise(y, m, filter='moving average'):
     """
-    Decodes the signal under utf-8 transmitter design:
-    Takes the sign of the average over 2*N0/3 transmissions of the same bit
+    Filter the noisy signal to remove noise
+    Parameters:
+        y: signal to filter
+        m: window size
     """
-    if N0 % 3 != 0:
-        raise ValueError('Warning: N0 should be multiple of 3')
-    N0_eff = 2*N0//3
-
-    out, n = load_signal(N0)
-
-    out_clean = [out[i] for i in range(len(out)) if i % 3 != H_hat]
-    if len(out_clean) % N0_eff != 0:
-        raise ValueError('Warning: length of out_clean should be multiple of N0_eff')
-
-    averages = [sum(out_clean[k*N0_eff : (k+1)*N0_eff]) for k in range(n//N0)]
+    # Careful: filter reduces size of y, pad y with 0s to keep same size
     
-    # Map to -1, 0 or 1
-    decoded_bits = np.sign(np.array(averages))
-    
-    # Map to binary
-    decoded_bits = ((decoded_bits+1)/2).astype(int)
-    decoded_ints = [int(''.join(decoded_bits[8*i:8*(i+1)].astype(str)), 2) for i in range(len(decoded_bits)//8)]
+    if filter == 'moving average':
+        # Moving average taken from: https://stackoverflow.com/a/14314054
+        ret = np.cumsum(y, dtype=float)
+        ret[m:] = ret[m:] - ret[:-m]
+        return ret[m - 1:] / m
+
+def get_charac(w_hat, psi, T_min, T_max, T):
+    """
+    Performs w_hat dot psi_j for every j to recover the bits
+    """
+    bits = [0]*7
+    N0_eff = 2*(T_max-T_min)/T//3
+    for k in range(7):
+        signal = np.array(w_hat[k*N0:(k+1)*N0])
+        dot_product = signal.dot(np.array([psi(j*T) for j in range(N0_eff//7)]))
+        bits[k] = (np.sign(dot_product)+1)/2
+        
+    decoded_ints = [int(''.join(bits[7*i:7*(i+1)].astype(str)), 2) for i in range(len(bits)//7)]
     decoded_bytes = bytes(decoded_ints)
     
-    print(f"Decoded bytes: {decoded_bytes}", f"{len(decoded_bytes)} bytes")
-
-    decoded_string = decoded_bytes.decode('utf-8')
-
-    return decoded_string
-
-def decode_utf8_8bits(N0, H_hat):
-    """
-    Decodes the signal under an utf-8 transmitter design optimized for 8bits utf-8:
-    Takes the sign of the average over 2*N0/3 transmissions of the same bit
-    """
-    if N0 % 3 != 0:
-        raise ValueError('Warning: N0 should be multiple of 3')
-    N0_eff = 2*N0//3
-
-    out, n = load_signal(N0)
-
-    out_clean = [out[i] for i in range(len(out)) if i % 3 != H_hat]
-    if len(out_clean) % N0_eff != 0:
-        raise ValueError('Warning: length of out_clean should be multiple of N0_eff')
-
-    averages = [sum(out_clean[k*N0_eff : (k+1)*N0_eff]) for k in range(n//N0)]
-
-    # Map to -1, 0 or 1
-    decoded_bits = np.sign(np.array(averages))
-
-    # Map to binary
-    decoded_bits = ((decoded_bits+1)/2).astype(int)
-
-    decoded_ints = [int(''.join(decoded_bits[7*i:7*(i+1)].astype(str)), 2) for i in range(len(decoded_bits)//7)]
-    decoded_bytes = bytes(decoded_ints)
-
-    print(f"Decoded bytes:\n{decoded_bytes}", f"{len(decoded_bytes)} bytes")
-
-    decoded_string = decoded_bytes.decode('utf-8')
-
-    return decoded_string
-
-dic = {
-    '1000': "e",
-    '1001': " ",
-    '1010': "t",
-    '1011': "a",
-    '1100': "o",
-    '1101': "i",
-    '1110': "n",
-    '1111': "s"
-    }
-
-def decode_with_dic(N0, H_hat, dic):
-    if N0 % 3 != 0:
-        raise ValueError('Warning: N0 should be multiple of 3')
-    N0_eff = 2*N0//3
-
-    out, n = load_signal(N0)
-
-    out_clean = [out[i] for i in range(len(out)) if i % 3 != H_hat]
-    if len(out_clean) % N0_eff != 0:
-        raise ValueError('Warning: length of out_clean should be multiple of N0_eff')
-
-    averages = [sum(out_clean[k*N0_eff : (k+1)*N0_eff]) for k in range(n//N0)]
-
-    # Map to -1, 0 or 1
-    decoded_bits = np.sign(np.array(averages))
-
-    # Map to binary
-    decoded_bits = ((decoded_bits+1)/2).astype(int)
-
-    # Decode using a 3-states automaton
-    decoded_string = []
-    state = 'expecting first bit'
-    collect_8 = collect_4 = []
-
-    for b in decoded_bits:
-        current_state = state
-        if current_state == 'expecting first bit':
-            if b==1:
-                state = 'expecting 4'
-                collect_4 = [1]
-            if b==0:
-                state = 'expecting 8'
-                collect_8 = [0]
-        elif current_state == 'expecting 4':
-            collect_4.append(b)
-            if len(collect_4) == 4:
-                pack4 = ''.join(list(map(str,collect_4)))
-                decoded_string.append(dic[pack4])
-                state = 'expecting first bit'
-        elif current_state == 'expecting 8':
-                collect_8.append(b)
-                if len(collect_8) == 8:
-                   decoded_int = bin_to_dec(collect_8)
-                   decoded_byte = decoded_int.to_bytes(1,ENDIANNESS)
-                   decoded_string.append(decoded_byte.decode('utf8'))
-                   state = 'expecting first bit'
-
-    print("Finished decoding with state: {}\n".format(state))
-
-    return ''.join(decoded_string)    
-
-
-index, H_MAP = estimate_H()
-print("Deleted index: {} (from the following estimate: {})\n".format(index, H_MAP))
-
-decoded_string = decode_with_dic(N0, index, dic)
-print("Decoded string using N0={}:".format(N0), decoded_string)
-
-original_message = None
-with open('message.txt', 'rb') as f:
-    original_message = f.read().decode('utf-8').strip()
-
-if original_message:
-    print("Original message was: '", original_message, "'", sep='')
-
-    errors = dict()
-    for i in range(len(original_message)):
-        if original_message[i] != decoded_string[i]:
-            errors[i] = {'original': original_message[i], 'decoded': decoded_string[i] }
-
-    print(f"Total number of errors: {len(errors)}")
-    for i,e in errors.items():
-        print(f' - index {i}:')
-        print('\texpected this: {0:08b}'.format(ord(e["original"])), end='')
-        print(f' -> {e["original"].encode("utf-8")} -> {e["original"]}')
-        print('\tand got this : {0:08b}'.format(ord(e["decoded"])), end='')
-        print(f' -> {e["decoded"].encode("utf-8")} -> {e["decoded"]}')
+    return decoded_bits.decode('utf-8')
